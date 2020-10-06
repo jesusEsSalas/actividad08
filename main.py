81a687a5e1ce6e5f58cb87cddc39dcfbb9f43c6f
Jesus Salas <jesuse.salas@alumnos.udg.mx>
2020-10-06 09:12:54 -0500

    Borré comentarios

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import sys
 from mainwindow import MainWindow
 
 #pyside2-uic mainwindow.ui > ui_mainwindow.py
-# Aplicación de Qt
 app = QApplication()
 window = MainWindow()
 window.show()
-# Qt loop
 sys.exit(app.exec_())
